a828ab9b0feba84656141020a524bb1cfc6bab71
garugan <196047075+garugan@users.noreply.github.com>
2025-03-03 19:03:08 +0000

    bookers-validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book  = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -19,13 +23,19 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    if @book = Book.find(params[:id])
+      flash[:notice] = "Book was successfully updated."
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy

9e477415c8b96f6eb2fc7222759a5cf7685dff23
garugan <196047075+garugan@users.noreply.github.com>
2025-03-03 20:06:26 +0000

    bookers-flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book  = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -24,13 +25,13 @@ class BooksController < ApplicationController
 
   def edit
     if @book = Book.find(params[:id])
-      flash[:notice] = "Book was successfully updated."
     end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -41,6 +42,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
